ac9cc6e5702fa8ab12f7967caf55692465e28106
sunamo.cz <radek.jancik@sunamo.cz>
2024-12-28 16:41:10 +0100

    24.12.28.1 Added class

---
+++ ResultWithExceptionList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SunamoResult;
+public class ResultWithExceptionList<T> : List<ResultWithException<T>>
+{
+    /// <summary>
+    /// Všechny chyby se musí logovat přesně tam kde vznikly
+    /// Tohle už slouží jen abych věděl že data jsou nekompletní. 
+    /// </summary>
+    public bool WasSomeError { get => this.Any(d => d.Exc != null); }
+}
