c074cc23267e1e5c9d212b780fc7b145eeef7eb1
sunamo.cz <radek.jancik@sunamo.cz>
2025-03-05 07:29:20 +0100

    Added global using of Microsoft.Extensions.Logging.Abstractions to all projects

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -8,3 +8,8 @@ global using System.Reflection;
 global using System.Net;
 global using System.Diagnostics;
 global using SunamoResult;
+
+global using System.Threading.Tasks;
+global using ILogger = Microsoft.Extensions.Logging.ILogger;
+global using Microsoft.Extensions.Logging;
+global using NullLogger = Microsoft.Extensions.Logging.Abstractions.NullLogger;
